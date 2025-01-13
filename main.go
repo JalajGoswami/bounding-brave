44f81264d680bb4b8a33c9fd57e34c2e96a1cf87
Jalaj Goswami <jalaj.goswami@buopso.com>
2025-01-13 13:29:40 +0530

    project initiated with ebitengine

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+type Game struct {
+}
+
+func (g *Game) Update() error {
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return 640, 360
+}
+
+func main() {
+	ebiten.SetWindowSize(1280, 720)
+	ebiten.SetWindowTitle("Bounding Brave")
+
+	if err := ebiten.RunGame(&Game{}); err != nil {
+		log.Fatal(err)
+	}
+}
