dd2d1b3809c1d9f2dad03aeb7ac5513f744de7de
Jalaj Goswami <jalajgoswami33@gmail.com>
2025-02-10 08:43:56 +0000

    collision logic done

--- config/rendering.go
+++ config/rendering.go
@@ -4,3 +4,7 @@ const SCREEN_WIDTH = 640
 const SCREEN_HEIGHT = 360
 
 const PIXED_DENSITY = 2
+
+// ranges from 0-999 its value increment after each frame,
+// can be used to generate subsets like 0-59 by Tick%60
+var Tick = 0
